bf76649708441490cfcf736d55f7b72c6de1a029
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-07-15 18:31:59 +0600

    Declare SwitchCase

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
+<!-- Passing Data -->
 <h1>Sum is={{$marks}}</h1>
 
-
+<!-- If-else -->
 @if($marks>=80 && $marks<=100)
 <h1>Grade: A+</h1>
 
@@ -14,4 +15,21 @@
  <h1>Grade: F</h1>
  @endif
 
+ <!-- Asset Binding -->
  <img src="{{asset('https://captainbinary.com/images/hero-img.svg')}}">
+
+ <!-- Switch Case -->
+  @switch($marks)
+
+  @case(100)
+  <h1>A+</h1>
+  @break
+
+  @case(80)
+  <h1>A</h1>
+  @break
+
+  @default
+  <h1>Do not know</h1>
+
+  @endswitch()
